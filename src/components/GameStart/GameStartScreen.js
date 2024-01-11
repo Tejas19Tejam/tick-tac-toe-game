00fd2b162cc1b9454c72bc410b9506393aba723c
Vasudev Tejam <vasudevtejam@outlook.com>
2024-01-11 09:06:16 +0530

    Delete unused files
    Delete unused files

--- src/components/GameStart/GameStartScreen.js
+++ src/components/GameStart/GameStartScreen.js
@@ -1,7 +1,3 @@
-import GameLogo from '../../ui/GameLogo';
-import PlayerSelector from './PlayerSelector';
-import GameModeSelector from './GameModeSelector';
-
 function GameStartScreen({ children }) {
 	return <div className='start-screen'>{children}</div>;
 }
