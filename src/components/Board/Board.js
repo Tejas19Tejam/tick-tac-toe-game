c76d6542384da4d08aa2bca956d123fba299c685
Vasudev Tejam <vasudevtejam@outlook.com>
2024-01-11 09:15:16 +0530

    Environment variable added

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import Square from './Square';
 import { makeComputerMove } from '../../utils/helper';
